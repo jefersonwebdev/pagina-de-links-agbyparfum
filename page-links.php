518e1658f0d2fbf20548bcda6a364673adc0a8b8
Jeferson Guimarães <jeferson.phelipe@gmail.com>
2026-03-15 16:02:07 -0300

    alteracao dos links

--- page-links.php
+++ page-links.php
@@ -145,15 +145,15 @@ Template Name: Página de Links Customizada
 
     <div class="links-container">
         <a href="https://agbyparfum.com.br/" class="link-card" target="_blank">
-            <span>SITE OFICIAL</span>
+            <span>LOJA VIRTUAL</span>
         </a>
         
-        <a href="#" class="link-card" target="_blank">
-            <span>LANÇAMENTOS EXCLUSIVOS</span>
+        <a href="https://www.instagram.com/ag_byparfum/" class="link-card" target="_blank">
+            <span>INSTAGRAM</span>
         </a>
 
-        <a href="https://agbyparfum.com.br/" class="link-card" target="_blank">
-            <span>LOJA VIRTUAL</span>
+        <a href="https://www.tiktok.com/@ag_byparfum" class="link-card" target="_blank">
+            <span>TIK TOK</span>
         </a>
 
         <a href="https://wa.me/5513991486732?text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20seus%20produtos.%20Vim%20pela%20p%C3%A1gina%20de%20links." class="link-card" target="_blank">
@@ -161,17 +161,20 @@ Template Name: Página de Links Customizada
         </a>
     </div>
 
+    <!--
     <div class="social-icons">
         <a href="https://www.instagram.com/ag_byparfum/" aria-label="Instagram">
             <svg viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.209-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
         </a>
-        <!--
+        
         <a href="#" aria-label="Facebook">
-            <svg viewBox="0 0 24 24"><path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.323-1.325z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+            </svg>
         </a>
-        -->
+        
     </div>
-
+    -->
     <footer class="mt-auto py-6 text-[10px] opacity-40 uppercase tracking-[2px]">
         &copy; 2026 AGBYPARFUM - Todos os direitos reservados
     </footer>
